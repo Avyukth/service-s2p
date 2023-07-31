67539c8fe8855e6cb74c0d39c5f0403317908525
Avyukth <makur.subhrajit@gmail.com>
2023-07-31 20:55:25 +0530

    log core created

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,28 +2,47 @@ package main
 
 import (
 	"fmt"
-	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"log"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 )
 
 var build = "develop"
 
 func main() {
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Println("maxprocs: %w", err)
+
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	g := runtime.GOMAXPROCS(0)
-	log.Printf("Starting the sales API build[%s] CPU[%d].............", build, g)
-	defer log.Println("Service Ended")
+	defer log.Sync()
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
 
-	log.Panicln("stopping Service ........")
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.ErrorOutputPaths = []string{"stderr"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/Avyukth/service3-clone
 
 go 1.20
 
-require go.uber.org/automaxprocs v1.5.3
+require go.uber.org/zap v1.24.0
+
+require (
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+)
