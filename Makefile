a2fbacafacee0296a594a23fcb02bb7b29e5c795
Avyukth <makur.subhrajit@gmail.com>
2023-08-01 10:25:20 +0530

    log fmt completed

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ KIND_CLUSTER    := subhrajit-starter-cluster
 KIND            := kindest/node:v1.27.3
 
 run:
-	go run app/services/sales-api/main.go
+	go run app/services/sales-api/main.go  |  go run app/tooling/logfmt/main.go
 
 
 all: sales-api
--- app/tooling/logfmt/main.go
+++ app/tooling/logfmt/main.go
@@ -1,3 +1,4 @@
+// This program takes the structured log output and makes it readable.
 package main
 
 import (
@@ -17,14 +18,15 @@ func init() {
 }
 
 func main() {
-
 	flag.Parse()
 	var b strings.Builder
 
 	service := strings.ToLower(service)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		s := scanner.Text()
+
 		m := make(map[string]any)
 		err := json.Unmarshal([]byte(s), &m)
 		if err != nil {
@@ -33,10 +35,13 @@ func main() {
 			}
 			continue
 		}
+
+		// If a service filter was provided, check.
 		if service != "" && strings.ToLower(m["service"].(string)) != service {
 			continue
 		}
 
+		// I like always having a traceid present in the logs.
 		traceID := "00000000-0000-0000-0000-000000000000"
 		if v, ok := m["trace_id"]; ok {
 			traceID = fmt.Sprintf("%v", v)
@@ -46,9 +51,15 @@ func main() {
 
 		// Build out the know portions of the log in the order
 		// I want them in.
-
 		b.Reset()
-		b.WriteString(fmt.Sprintf("%s: %s: %s: %s: %s: %s: ", m["service"], m["time"], m["file"], m["level"], traceID, m["msg"]))
+		b.WriteString(fmt.Sprintf("%s: %s: %s: %s: %s: %s: ",
+			m["service"],
+			m["ts"],
+			m["caller"],
+			m["level"],
+			traceID,
+			m["msg"],
+		))
 
 		// Add the rest of the keys ignoring the ones we already
 		// added for the log.
@@ -57,15 +68,17 @@ func main() {
 			case "service", "time", "file", "level", "trace_id", "msg":
 				continue
 			}
+
 			// It's nice to see the key[value] in this format
 			// especially since map ordering is random.
-			b.WriteString(fmt.Sprintf("%s[%v]:", k, v))
+			b.WriteString(fmt.Sprintf("%s[%v]: ", k, v))
 		}
+
 		// Write the new log format, removing the last :
 		out := b.String()
 		fmt.Println(out[:len(out)-2])
-
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Println(err)
 	}
