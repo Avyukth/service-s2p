67fab7b40aadb398e58e076edc54676e86c4b2f4
Avyukth <makur.subhrajit@gmail.com>
2023-08-04 08:29:03 +0530

    apimux added in handler

--- app/services/sales-api/handlers/handllers.go
+++ app/services/sales-api/handlers/handllers.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"expvar"
-	"github.com/dimfeld/httptreemux/v5"
-	"go.uber.org/zap"
 	"net/http"
 	"net/http/pprof"
 	"os"
+
+	"github.com/dimfeld/httptreemux/v5"
+	"go.uber.org/zap"
 )
 
 type APIMuxConfig struct {
@@ -27,6 +29,14 @@ func DebugStandardLibraryMux() *http.ServeMux {
 
 func APIMux(cfg APIMuxConfig) http.Handler {
 	mux := httptreemux.NewContextMux()
-
+	h := func(w http.ResponseWriter, r *http.Request) {
+		status := struct {
+			Status string `json:"status"`
+		}{
+			Status: "OK",
+		}
+		json.NewEncoder(w).Encode(status)
+	}
+	mux.Handle(http.MethodGet, "/test", h)
 	return mux
 }
