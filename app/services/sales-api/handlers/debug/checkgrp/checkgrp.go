5ce24cbca95e5ab638a09001b28f10c455a35ea0
Avyukth <makur.subhrajit@gmail.com>
2023-08-04 10:55:06 +0530

    adding liveness code complete

--- app/services/sales-api/handlers/debug/checkgrp/checkgrp.go
+++ app/services/sales-api/handlers/debug/checkgrp/checkgrp.go
@@ -3,6 +3,7 @@ package checkgrp
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
 	"go.uber.org/zap"
 )
@@ -14,10 +15,8 @@ type Handlers struct {
 
 func (h *Handlers) Readiness(w http.ResponseWriter, r *http.Request) {
 	data := struct {
-		Build  string `json:"build"`
 		Status string `json:"status"`
 	}{
-		Build:  h.Build,
 		Status: "OK",
 	}
 	statusCode := http.StatusOK
@@ -29,7 +28,33 @@ func (h *Handlers) Readiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handlers) Liveness(w http.ResponseWriter, r *http.Request) {
+	host, err := os.Hostname()
+	if err != nil {
+		host = "unavailable"
+	}
+	data := struct {
+		Status    string `json:"status,omitempty"`
+		Build     string `json:"build,omitempty"`
+		Host      string `json:"host,omitempty"`
+		Pod       string `json:"pod,omitempty"`
+		PodIP     string `json:"podIP,omitempty"`
+		Node      string `json:"node,omitempty"`
+		NameSpace string `json:"namespace,omitempty"`
+	}{
 
+		Status:    "up",
+		Build:     h.Build,
+		Host:      host,
+		Pod:       os.Getenv("KUBERNETES_POD_NAME"),
+		PodIP:     os.Getenv("KUBERNETES_POD_IP"),
+		Node:      os.Getenv("KUBERNETES_NODE_NAME"),
+		NameSpace: os.Getenv("KUBERNETES_NAMESPACE"),
+	}
+	statusCode := http.StatusOK
+	if err := response(w, statusCode, data); err != nil {
+		h.Log.Errorw("liveness", "ERROR", err)
+	}
+	h.Log.Infow("liveness", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
 }
 
 func response(w http.ResponseWriter, statusCode int, data interface{}) error {
