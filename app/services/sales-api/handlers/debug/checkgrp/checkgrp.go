2b13c461ea1b626a2ee132df37088cc699cdee84
Avyukth <makur.subhrajit@gmail.com>
2023-08-04 08:51:13 +0530

    adding readiness and liveness code

---
+++ app/services/sales-api/handlers/debug/checkgrp/checkgrp.go
@@ -0,0 +1,44 @@
+package checkgrp
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+type Handlers struct {
+	Build string
+	Log   *zap.SugaredLogger
+}
+
+func (h *Handlers) Readiness(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Status string `json:"status"`
+	}{
+		Status: "OK",
+	}
+	statusCode := http.StatusOK
+	if err := response(w, statusCode, data); err != nil {
+		h.Log.Errorw("readiness", "ERROR", err)
+	}
+
+	h.Log.Infow("readiness", "statusCode", data.Status)
+}
+
+func (h *Handlers) Liveness(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func response(w http.ResponseWriter, statusCode int, data interface{}) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if _, err := w.Write(jsonData); err != nil {
+		return err
+	}
+	return nil
+}
