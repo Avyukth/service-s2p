4aca478fa858fde11f11510674c3fc66817d19d0
Avyukth <makur.subhrajit@gmail.com>
2023-08-06 00:52:57 +0530

    context.go added

---
+++ business/web/mid/context.go
@@ -0,0 +1,44 @@
+package mid
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+type ctxKey int
+
+const (
+	key ctxKey = iota
+)
+
+type Values struct {
+	TraceID    string
+	Now        time.Time
+	StatusCode int
+}
+
+func GetValues(ctx context.Context) (*Values, error) {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return nil, errors.New("web value missing from context")
+	}
+	return v, nil
+}
+
+func GetTraceID(ctx context.Context) string {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return "00000000-0000-0000-0000-000000000000"
+	}
+	return v.TraceID
+}
+
+func GetStatusCode(ctx context.Context, statusCode int) error {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return errors.New("web value missing from context")
+	}
+	v.StatusCode = statusCode
+	return nil
+}
