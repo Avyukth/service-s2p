3273cbdeacd7f9bae8c394cffd7ae09b4ca839ae
Avyukth <makur.subhrajit@gmail.com>
2023-08-05 10:27:11 +0530

    added middleware

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -14,17 +14,26 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	*httptreemux.ContextMux
 	shutdown chan os.Signal
+	mw       []Middleware
 }
 
-func NewApp(shutdown chan os.Signal) *App {
-	return &App{ContextMux: httptreemux.NewContextMux(), shutdown: shutdown}
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
+	return &App{
+		ContextMux: httptreemux.NewContextMux(),
+		shutdown:   shutdown,
+		mw:         mw,
+	}
 }
 
 func (a *App) SignalShutdown() {
 	a.shutdown <- syscall.SIGTERM
 }
 
-func (a *App) Handle(method string, group string, path string, handler Handler) {
+func (a *App) Handle(method string, group string, path string, handler Handler, mw ...Middleware) {
+
+	handler = wrapMiddleware(mw, handler)
+
+	handler = wrapMiddleware(a.mw, handler)
 
 	h := func(w http.ResponseWriter, r *http.Request) {
 		// PRE CODE  PROCESSING
