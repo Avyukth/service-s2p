03d759c4d9ae9fa408a8f7c7a8c83d77e360ce68
Avyukth <makur.subhrajit@gmail.com>
2023-08-07 09:43:07 +0530

    added errors middleware

--- app/services/sales-api/handlers/v1/testgrp/trestgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/trestgrp.go
@@ -2,6 +2,7 @@ package testgrp
 
 import (
 	"context"
+	"errors"
 	"math/rand"
 	"net/http"
 
@@ -16,7 +17,7 @@ type Handlers struct {
 
 func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Intn(100); n%2 == 0 {
-		return validate.NewRequestError(errors.New("untrusted error"), http.StatusBadRequest)
+		return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
 	}
 
 	status := struct {
