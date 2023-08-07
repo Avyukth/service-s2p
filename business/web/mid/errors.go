b33c4246d4a22750aea8161927ac1967c33f4714
Avyukth <makur.subhrajit@gmail.com>
2023-08-07 09:21:57 +0530

    typo

--- business/web/mid/errors.go
+++ business/web/mid/errors.go
@@ -9,54 +9,54 @@ import (
 	"go.uber.org/zap"
 )
 
-func Errors(log *zap.SugaredLogger) web.Middleware{
+func Errors(log *zap.SugaredLogger) web.Middleware {
 
-	m:= func(next web.Handler) web.Handler {
-		h:= func (ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	m := func(handler web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 			v, err := web.GetValues(ctx)
-			if err!= nil {
-                return web.NewShutdownError("Web values missing from context")
-				
+			if err != nil {
+				return web.NewShutdownError("Web values missing from context")
+
 			}
 
-			if err := handler(ctx, w,r); err!=nil {
-			log.Errorw("ERROR", "traceid", v.TraceID, "ERROR", err)
-			var er validate.ErrorResponse
-			var status int 
-			switch  act:= validate.Cause(err).(type) {
+			if err := handler(ctx, w, r); err != nil {
+				log.Errorw("ERROR", "traceid", v.TraceID, "ERROR", err)
+				var er validate.ErrorResponse
+				var status int
+				switch act := validate.Cause(err).(type) {
 
-			case *validate.FieldErrors:
-				er = validate.ErrorResponse{
-					Error: "data validation error",
-					Fields: act.Error(),
+				case *validate.FieldErrors:
+					er = validate.ErrorResponse{
+						Error:  "data validation error",
+						Fields: act.Error(),
+					}
+					status = http.StatusBadRequest
+
+				case *validate.RequestError:
+					er = validate.ErrorResponse{
+						Error: act.Error(),
+					}
+					status = act.Status
+				default:
+					er = validate.ErrorResponse{
+						Error: http.StatusText(http.StatusInternalServerError),
+					}
+					status = http.StatusInternalServerError
+
 				}
-			status = http.StatusBadRequest
 
-			case *validate.RequestError:
-						er = validate.ErrorResponse{
-							Error : act.Error(),
-						}
-						status = act.Status 
-			default:	
-			er= validate.ErrorResponse{
-				Error: http.StatusText(http.StatusInternalServerError),
-				}				
-				status = http.StatusInternalServerError
-				
-			}
-			
-			if err:= web.Respond(ctx, w, status, er); err!= nil {
-				return err
-			}
+				if err := web.Respond(ctx, w, status, er); err != nil {
+					return err
+				}
 
-			if ok := web.IsShutdown(err); ok {
-				return err
+				if ok := web.IsShutdown(err); ok {
+					return err
+				}
 			}
-				
-				return nil
+			return nil
 		}
 		return h
 	}
+	return m
 
-		return m
 }
