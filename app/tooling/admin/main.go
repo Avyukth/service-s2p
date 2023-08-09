ddda962caf18aaf8533cba0eb5da3409424bfacc
Avyukth <makur.subhrajit@gmail.com>
2023-08-10 02:30:30 +0530

    adding logic for jwt to be validated at server side

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -5,12 +5,14 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -22,6 +24,44 @@ func main() {
 }
 
 func genToken() error {
+	//	==================================================================================
+	// Get private key file from Disk.
+
+	name, err := genPrivateKey()
+	if err != nil {
+		return fmt.Errorf("error getting private key name: %w", err)
+	}
+
+	// file, err := os.Open(name)
+	// if err != nil {
+	// 	return err
+	// }
+
+	privateKeyFile, err := os.OpenFile(name, os.O_RDONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("creating private.pem file: %w", err)
+	}
+
+	// defer privateKeyFile.Close()
+
+	privatePEM, err := io.ReadAll(io.LimitReader(privateKeyFile, 1024*1024))
+
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	fmt.Println("================================================================== private Key")
+	fmt.Println(string(privatePEM))
+	fmt.Println("==================================================================")
+
+	// ============================================================================================================================
+	// generate token from claim
+
 	claims := struct {
 		jwt.RegisteredClaims `json:"registered"`
 		Roles                []string `json:"roles"`
@@ -42,48 +82,140 @@ func genToken() error {
 	token := jwt.NewWithClaims(method, claims.RegisteredClaims)
 	token.Header["kid"] = "995fcf0b-73d9-4516-a32f-4e85d723f06d"
 
-	name := "zarf/keys/995fcf0b-73d9-4516-a32f-4e85d723f06d.pem"
-
-	file, err := os.Open(name)
-	if err != nil {
-		return err
-	}
-
-	privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
-
-	if err != nil {
-		return fmt.Errorf("reading auth private key: %w", err)
-	}
-
-	privateKey, err := jwt.ParseECPrivateKeyFromPEM(privatePEM)
-	if err != nil {
-		return fmt.Errorf("parsing auth private key: %w", err)
-	}
-
-	ss, err := token.SignedString(privateKey)
+	tokenStr, err := token.SignedString(privateKey)
 	if err != nil {
 		return fmt.Errorf("signing token: %w", err)
 	}
 
-	fmt.Println("====================TOKEN STARTED======================")
-	fmt.Println(ss)
+	fmt.Println("==================== TOKEN STARTED ======================")
+	fmt.Println(tokenStr)
 
-	fmt.Println("====================TOKEN ENDED======================")
+	fmt.Println("==================== TOKEN ENDED ======================")
 
+	fmt.Println("\n")
+
+	// =================================================================================================================
+	// write to standard out the public key
+
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %v", err)
+	}
+
+	fmt.Println("private and public keys generated successfully")
+	// =================================================================================================================
+
+	parser := &jwt.Parser{}
+
+	keyFunc := func(t *jwt.Token) (interface{}, error) {
+		kid, ok := t.Header["kid"]
+		if !ok {
+			return nil, errors.New("missing key id (kid) in token header ")
+		}
+
+		kidID, ok := kid.(string)
+		if !ok {
+			return nil, errors.New("user token key id (kid) must be string")
+
+		}
+		fmt.Println("========== kid:", kidID)
+
+		return &privateKey.PublicKey, nil
+	}
+
+	var parsedClaims = struct {
+		jwt.RegisteredClaims `json:"registered"`
+		Roles                []string `json:"roles"`
+	}
+
+	parsedToken, err := parser.ParseWithClaims(tokenStr, &parsedClaims, keyFunc)
+	if err != nil {
+		return fmt.Errorf("parsing token: %w", err)
+	}
+	if !parsedToken.Valid {
+		return fmt.Errorf("token is not valid")
+	}
+
+	fmt.Println("========== Token valided:")
 	return nil
 }
 
-func genKey() error {
+// func genKey() error {
+
+// 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+// 	if err != nil {
+// 		return err
+// 	}
+// 		name := "zarf/keys/" + uuid.New().String() + ".pem"
+// 	privateKeyFile, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+
+// 	// privateKeyFile, err := os.OpenFile("private.pem", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+// 	if err != nil {
+// 		return fmt.Errorf("creating private.pem file: %w", err)
+// 	}
+
+// 	defer privateKeyFile.Close()
+
+// 	privateBlock := pem.Block{
+// 		Type:  "RSA PRIVATE KEY",
+// 		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+// 	}
+
+// 	if err := pem.Encode(privateKeyFile, &privateBlock); err != nil {
+// 		return fmt.Errorf("encoding to private file: %v", err)
+// 	}
+
+// 	// ============================================================================================================================
+// 	// Public Pem file creation
+
+// 	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+
+// 	if err != nil {
+// 		return fmt.Errorf("marshaling public key: %w", err)
+// 	}
+
+// 	publicKeyFile, err := os.OpenFile("public.pem", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+
+// 	if err != nil {
+// 		return fmt.Errorf("creating public.pem file: %w", err)
+// 	}
+// 	defer publicKeyFile.Close()
+
+// 	publicBlock := pem.Block{
+// 		Type:  "RSA PUBLIC KEY",
+// 		Bytes: asn1Bytes,
+// 	}
+
+// 	if err := pem.Encode(publicKeyFile, &publicBlock); err != nil {
+// 		return fmt.Errorf("encoding to public file: %v", err)
+// 	}
+
+// 	fmt.Println("private and public keys generated successfully")
+// 	return nil
+// }
+
+func genPrivateKey() (string, error) {
 
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 
 	if err != nil {
-		return err
+		return "", err
 	}
-
-	privateKeyFile, err := os.OpenFile("private.pem", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+	name := "zarf/keys/" + uuid.New().String() + ".pem"
+	privateKeyFile, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
-		return fmt.Errorf("creating private.pem file: %w", err)
+		return "", fmt.Errorf("creating private.pem file: %w", err)
 	}
 
 	defer privateKeyFile.Close()
@@ -94,34 +226,10 @@ func genKey() error {
 	}
 
 	if err := pem.Encode(privateKeyFile, &privateBlock); err != nil {
-		return fmt.Errorf("encoding to private file: %v", err)
+		return "", fmt.Errorf("encoding to private file: %v", err)
 	}
-
+	fmt.Println("==================================pem generate")
+	return name, nil
 	// ============================================================================================================================
-	// Public Pem file creation
 
-	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
-
-	if err != nil {
-		return fmt.Errorf("marshaling public key: %w", err)
-	}
-
-	publicKeyFile, err := os.OpenFile("public.pem", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-
-	if err != nil {
-		return fmt.Errorf("creating public.pem file: %w", err)
-	}
-	defer publicKeyFile.Close()
-
-	publicBlock := pem.Block{
-		Type:  "RSA PUBLIC KEY",
-		Bytes: asn1Bytes,
-	}
-
-	if err := pem.Encode(publicKeyFile, &publicBlock); err != nil {
-		return fmt.Errorf("encoding to public file: %v", err)
-	}
-
-	fmt.Println("private and public keys generated successfully")
-	return nil
 }
