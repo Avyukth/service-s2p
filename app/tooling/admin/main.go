a21e4390521c1dc75921da455bd3bfc79a081da7
Avyukth <makur.subhrajit@gmail.com>
2023-08-13 18:31:14 +0530

    remove commented code

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -150,61 +150,6 @@ func genToken() error {
 	return nil
 }
 
-// func genKey() error {
-
-// 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-
-// 	if err != nil {
-// 		return err
-// 	}
-// 		name := "zarf/keys/" + uuid.New().String() + ".pem"
-// 	privateKeyFile, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-
-// 	// privateKeyFile, err := os.OpenFile("private.pem", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-// 	if err != nil {
-// 		return fmt.Errorf("creating private.pem file: %w", err)
-// 	}
-
-// 	defer privateKeyFile.Close()
-
-// 	privateBlock := pem.Block{
-// 		Type:  "RSA PRIVATE KEY",
-// 		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-// 	}
-
-// 	if err := pem.Encode(privateKeyFile, &privateBlock); err != nil {
-// 		return fmt.Errorf("encoding to private file: %v", err)
-// 	}
-
-// 	// ============================================================================================================================
-// 	// Public Pem file creation
-
-// 	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
-
-// 	if err != nil {
-// 		return fmt.Errorf("marshaling public key: %w", err)
-// 	}
-
-// 	publicKeyFile, err := os.OpenFile("public.pem", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-
-// 	if err != nil {
-// 		return fmt.Errorf("creating public.pem file: %w", err)
-// 	}
-// 	defer publicKeyFile.Close()
-
-// 	publicBlock := pem.Block{
-// 		Type:  "RSA PUBLIC KEY",
-// 		Bytes: asn1Bytes,
-// 	}
-
-// 	if err := pem.Encode(publicKeyFile, &publicBlock); err != nil {
-// 		return fmt.Errorf("encoding to public file: %v", err)
-// 	}
-
-// 	fmt.Println("private and public keys generated successfully")
-// 	return nil
-// }
-
 func genPrivateKey() (string, string, error) {
 
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
