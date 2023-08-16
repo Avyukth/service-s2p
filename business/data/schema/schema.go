93d300a5592c31ab10a324b28de6670b806d914e
Avyukth <makur.subhrajit@gmail.com>
2023-08-16 22:02:53 +0530

    adding migration script

--- business/data/schema/schema.go
+++ business/data/schema/schema.go
@@ -1,6 +1,14 @@
 package schema
 
-import _ "embed"
+import (
+	"context"
+	_ "embed"
+	"fmt"
+
+	"github.com/Avyukth/service3-clone/business/sys/database"
+	"github.com/ardanlabs/darwin"
+	"github.com/jmoiron/sqlx"
+)
 
 var (
 	//go:embed sql/schema.sql
@@ -9,3 +17,18 @@ var (
 	//go:embed sql/delete.sql
 	deleteDoc string
 )
+
+func Migrate(ctx context.Context, db *sqlx.DB) error {
+
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database error: %w", err)
+	}
+
+	driver, err := darwin.NewGenericDriver(db.DB, darwin.PostgresDialect{})
+	if err != nil {
+		return err
+	}
+
+	d := darwin.New(driver, darwin.ParseMigrations(schemaDoc))
+	return d.Migrate()
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.6
+	github.com/ardanlabs/darwin v1.5.0
 	github.com/dimfeld/httptreemux/v5 v5.5.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/google/uuid v1.3.0
