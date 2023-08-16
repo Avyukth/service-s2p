1b1851b1df98c0db904b2c47c6faac795cd29d42
Avyukth <makur.subhrajit@gmail.com>
2023-08-16 22:47:41 +0530

    adding seed  func

--- business/data/schema/schema.go
+++ business/data/schema/schema.go
@@ -14,6 +14,9 @@ var (
 	//go:embed sql/schema.sql
 	schemaDoc string
 
+	//go:embed sql/seed.sql
+	seedDoc string
+
 	//go:embed sql/delete.sql
 	deleteDoc string
 )
@@ -47,5 +50,25 @@ func DeleteALl(ctx context.Context, db *sqlx.DB) error {
 		return err
 	}
 
+	return tx.Commit()
+}
+
+func Seed(ctx context.Context, db *sqlx.DB) error {
+
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database error: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if _, err := tx.Exec(seedDoc); err != nil {
+		if err := tx.Rollback(); err != nil {
+			return err
+		}
+		return err
+	}
 	return tx.Commit()
 }
