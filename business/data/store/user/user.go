951224f64264e4df82c7cda8a619797d81e70e07
Avyukth <makur.subhrajit@gmail.com>
2023-08-17 13:11:33 +0530

    update and delete  logic

--- business/data/store/user/user.go
+++ business/data/store/user/user.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/Avyukth/service3-clone/business/sys/auth"
 	"github.com/Avyukth/service3-clone/business/sys/database"
 	"github.com/Avyukth/service3-clone/business/sys/validate"
 	"github.com/jmoiron/sqlx"
@@ -51,4 +52,87 @@ func (s Store) Create(ctx context.Context, nu *NewUser, now time.Time) (User, er
 		return User{}, fmt.Errorf("inserting user: %w", err)
 	}
 	return usr, nil
+}
+
+func (s Store) Update(ctx context.Context, claims auth.Claims, userID string, uu UpdateUser, now time.Time) error {
+
+	if err := validate.Check(userID); err != nil {
+		return database.ErrInvalidID
+	}
+
+	if err := validate.Check(uu); err != nil {
+		return fmt.Errorf("validating data: %w", err)
+	}
+
+	usr, err := s.QueryByID(ctx, claims, userID)
+	if err != nil {
+		return fmt.Errorf("updating user userID[%s]: %w", userID, err)
+	}
+
+	if uu.Name != nil {
+		usr.Name = *uu.Name
+	}
+	if uu.Email != nil {
+		usr.Email = *uu.Email
+	}
+	if uu.Roles != nil {
+		usr.Roles = uu.Roles
+	}
+
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return fmt.Errorf("generating password hash: %w", err)
+		}
+		usr.PasswordHash = pw
+
+	}
+	usr.DateUpdated = now
+	const q = `
+	UPDATE 
+		users 
+	SET 
+		"name"=:name, 
+		"email"=:email, 
+		"roles"=:roles, 
+		"date_updated"=:date_updated 
+	WHERE user_id=:user_id`
+	if err := database.NamedExecContext(ctx, s.log, s.db, q, usr); err != nil {
+		return fmt.Errorf("updating user userID[%s]: %w", userID, err)
+	}
+	return nil
+}
+
+func (s Store) Delete(ctx context.Context, claims auth.Claims, userID string) error {
+
+	if err := validate.Check(userID); err != nil {
+		return database.ErrInvalidID
+	}
+
+	if !claims.Authorized(auth.RoleAdmin) && claims.Subject != userID {
+		return database.ErrForbidden
+	}
+
+	data := struct {
+		UserID string `db:"user_id"`
+	}{
+		UserID: userID,
+	}
+
+	const q = `
+		DELETE FROM 
+			users 
+		WHERE 
+			user_id=:user_id`
+
+	if err := database.NamedExecContext(ctx, s.log, s.db, q, data); err != nil {
+		return fmt.Errorf("deleting user userID[%s]: %w", userID, err)
+	}
+
+	return nil
+}
+
+func (s Store) QueryByID(ctx context.Context, claims auth.Claims, userID string) error {
+
+	return nil
 }
