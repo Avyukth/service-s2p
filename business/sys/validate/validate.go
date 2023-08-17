2dca99f4437ebe0889c075b6f4d92859685509a5
Avyukth <makur.subhrajit@gmail.com>
2023-08-17 11:33:45 +0530

    adding custom validator to validate struct field

--- business/sys/validate/validate.go
+++ business/sys/validate/validate.go
@@ -16,13 +16,40 @@ var translator ut.Translator
 
 func init() {
 	validate = validator.New()
+
 	translator, _ = ut.New(en.New(), en.New()).GetTranslator("en")
+
 	en_translations.RegisterDefaultTranslations(validate, translator)
+
 	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
 		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+
 		if name == "-" {
 			return ""
 		}
+
 		return name
 	})
+}
+
+func Check(val interface{}) error {
+
+	if err := validate.Struct(val); err != nil {
+		verrors, ok := err.(validator.ValidationErrors)
+		if !ok {
+			return err
+		}
+
+		var fields FieldErrors
+		for _, verror := range verrors {
+			field := FieldError{
+				Field: verror.Field(),
+				Error: verror.Translate(translator),
+			}
+			fields = append(fields, field)
+		}
+		return fields
+	}
+
+	return nil
 }
