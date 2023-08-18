39ebe707f6cf70eeba28f85e1d36290780eab78e
Avyukth <makur.subhrajit@gmail.com>
2023-08-18 19:38:04 +0530

    moved location and changein models

--- business/data/store/user/models.go
+++ business/data/store/user/models.go
@@ -2,32 +2,30 @@ package user
 
 import (
 	"time"
-
-	"github.com/lib/pq"
 )
 
 type User struct {
-	ID           string         `db:"user_id" json:"id"`
-	Name         string         `db:"name" json:"name"`
-	Email        string         `db:"email" json:"email"`
-	Roles        pq.StringArray `db:"roles" json:"roles"`
-	PasswordHash []byte         `db:"password_hash" json:"-"`
-	DateCreated  time.Time      `db:"date_created" json:"date_created"`
-	DateUpdated  time.Time      `db:"date_updated" json:"date_updated"`
+	ID           string    `db:"user_id" json:"id"`
+	Name         string    `db:"name" json:"name"`
+	Email        string    `db:"email" json:"email"`
+	Roles        []Role    `db:"roles" json:"roles"`
+	PasswordHash []byte    `db:"password_hash" json:"-"`
+	DateCreated  time.Time `db:"date_created" json:"date_created"`
+	DateUpdated  time.Time `db:"date_updated" json:"date_updated"`
 }
 
 type NewUser struct {
-	Name            string   `json:"name" validate:"required"`
-	Email           string   `json:"email" validate:"required,email"`
-	Roles           []string `json:"roles" validate:"required"`
-	Password        string   `json:"password" validate:"required"`
-	PasswordConfirm string   `json:"password_confirm" validate:"eqfield=Password"`
+	Name            string `json:"name" validate:"required"`
+	Email           string `json:"email" validate:"required,email"`
+	Roles           []Role `json:"roles" validate:"required"`
+	Password        string `json:"password" validate:"required"`
+	PasswordConfirm string `json:"password_confirm" validate:"eqfield=Password"`
 }
 
 type UpdateUser struct {
-	Name            *string  `json:"name"`
-	Email           *string  `json:"email" validate:"omitempty,email"`
-	Roles           []string `json:"roles"`
-	Password        *string  `json:"password"`
-	PasswordConfirm *string  `json:"password_confirm" validate:"omitempty,eqfield=Password"`
+	Name            *string `json:"name"`
+	Email           *string `json:"email" validate:"omitempty,email"`
+	Roles           []Role  `json:"roles"`
+	Password        *string `json:"password"`
+	PasswordConfirm *string `json:"password_confirm" validate:"omitempty,eqfield=Password"`
 }
