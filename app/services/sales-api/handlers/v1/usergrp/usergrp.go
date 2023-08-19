d52786f12d9830c64eef9a80a8d34ac82a223bb9
Avyukth <makur.subhrajit@gmail.com>
2023-08-19 12:37:41 +0530

    added token generation part

--- app/services/sales-api/handlers/v1/usergrp/usergrp.go
+++ app/services/sales-api/handlers/v1/usergrp/usergrp.go
@@ -89,3 +89,94 @@ func (h Handlers) Create(ctx context.Context, w http.ResponseWriter, r *http.Req
 
 	return web.Respond(ctx, w, usr, http.StatusCreated)
 }
+
+func (h Handlers) Update(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return errors.New("claims missing in context")
+	}
+
+	var upd user.UpdateUser
+	if err := web.Decode(r, &upd); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+	id := web.Param(r, "id")
+	if err := h.User.Update(ctx, claims, id, upd, v.Now); err != nil {
+		switch validate.Cause(err) {
+		case database.ErrInvalidID:
+			return validate.NewRequestError(err, http.StatusBadRequest)
+		case database.ErrNotFound:
+			return validate.NewRequestError(err, http.StatusNotFound)
+		case database.ErrForbidden:
+			return validate.NewRequestError(err, http.StatusForbidden)
+		default:
+			return fmt.Errorf("ID[%s] User[%+v]: %w", id, &upd, err)
+		}
+	}
+
+	return web.Respond(ctx, w, nil, http.StatusNoContent)
+
+}
+
+func (h Handlers) Delete(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return errors.New("claims missing in context")
+	}
+
+	id := web.Param(r, "id")
+
+	if err := h.User.Delete(ctx, claims, id); err != nil {
+		switch validate.Cause(err) {
+		case database.ErrInvalidID:
+			return validate.NewRequestError(err, http.StatusBadRequest)
+		case database.ErrNotFound:
+			return validate.NewRequestError(err, http.StatusNotFound)
+		case database.ErrForbidden:
+			return validate.NewRequestError(err, http.StatusForbidden)
+		default:
+			return fmt.Errorf("ID[%s]: %w", id, err)
+		}
+	}
+	return web.Respond(ctx, w, nil, http.StatusNoContent)
+}
+
+func (h Handlers) Token(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	email, pass, ok := r.BasicAuth()
+	if !ok {
+		err := errors.New("must provide email and password in Basic auth")
+		return validate.NewRequestError(err, http.StatusUnauthorized)
+	}
+
+	claims, err := h.User.Authenticate(ctx, v.Now, email, pass)
+	if err != nil {
+		switch validate.Cause(err) {
+		case database.ErrNotFound:
+			return validate.NewRequestError(err, http.StatusNotFound)
+		case database.ErrAuthenticationFailure:
+			return validate.NewRequestError(err, http.StatusUnauthorized)
+		default:
+			return fmt.Errorf("email[%s]: %w", email, err)
+		}
+	}
+
+	var tkn struct {
+		Token string `json:"token"`
+	}
+
+	tkn.Token, err = h.Auth.GenerateToken(claims)
+
+	if err != nil {
+		return fmt.Errorf("unable to generate token: %w", err)
+	}
+	return web.Respond(ctx, w, tkn, http.StatusOK)
+}
