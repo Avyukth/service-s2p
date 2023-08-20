29165f735710b82d9f95fe868ff238b97d770767
Avyukth <makur.subhrajit@gmail.com>
2023-08-20 14:56:03 +0530

    adding graafana telemetry and tracing

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -8,13 +8,12 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/google/uuid"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.9.0"
 
 	"github.com/dimfeld/httptreemux/v5"
 )
@@ -22,20 +21,20 @@ import (
 type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 type App struct {
-	mux *httptreemux.ContextMux
-	otmux http.Handler
+	mux      *httptreemux.ContextMux
+	otmux    http.Handler
 	shutdown chan os.Signal
 	mw       []Middleware
 }
 
 func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
-	
+
 	mux := httptreemux.NewContextMux()
 	return &App{
-		mux: mux,
-		otmux: otelhttp.NewHandler(mux, "request"),
-		shutdown:   shutdown,
-		mw:         mw,
+		mux:      mux,
+		otmux:    otelhttp.NewHandler(mux, "request"),
+		shutdown: shutdown,
+		mw:       mw,
 	}
 }
 
@@ -43,8 +42,8 @@ func (a *App) SignalShutdown() {
 	a.shutdown <- syscall.SIGTERM
 }
 
-func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	a.otmux.ServeHTTP(w,r)
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.otmux.ServeHTTP(w, r)
 }
 
 func (a *App) Handle(method string, group string, path string, handler Handler, mw ...Middleware) {
@@ -53,11 +52,10 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 
 	handler = wrapMiddleware(a.mw, handler)
 
-
 	h := func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-	span:= trace.SpanFromContext(ctx)
+		span := trace.SpanFromContext(ctx)
 		v := Values{
 			TraceID: span.SpanContext().SpanID().String(),
 			Now:     time.Now(),
@@ -79,18 +77,17 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 	a.mux.Handle(method, finalPath, h)
 }
 
-
 // =============================================================================
 
 // startTracing configure open telemetry to be used with Grafana Tempo.
 
-func startTracing(serviceName string, reporterURI string, probability float64) (*trace.TracerProvider, error){
+func startTracing(serviceName string, reporterURI string, probability float64) (*trace.TracerProvider, error) {
 
 	exporter, err := otlptrace.New(
 		context.Background(),
 		otlptracegrpc.NewClient(
-		otlptracegrpc.WithInsecure(),
-		otlptracegrpc.WithEndpoint(reporterURI),
+			otlptracegrpc.WithInsecure(),
+			otlptracegrpc.WithEndpoint(reporterURI),
 		),
 	)
 
@@ -98,13 +95,19 @@ func startTracing(serviceName string, reporterURI string, probability float64) (
 		return nil, fmt.Errorf("creating new otel exporter: %w", err)
 	}
 
-	traceProvider := trace.NewNoopTracerProvider(
-		trace.WithSampler(trace.TraceIDRatioBased(probability),
-	trace.WithBatcher(exporter, trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
-	trace.WithBatchTimeout(trace.DefaultScheduleDelay*time.Millisecond),
-	trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),),
-trace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String(serviceName),),),
-	
-	
+	traceProvider := trace.NewTracerProvider(
+		trace.WithSampler(trace.TraceIDRatioBased(probability)),
+		trace.WithBatcher(exporter,
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+			trace.WithBatchTimeout(trace.DefaultScheduleDelay*time.Millisecond),
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+		),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(serviceName),
+			),
+		),
+	)
 
 }
--- go.mod
+++ go.mod
@@ -16,9 +16,10 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.2.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.42.0
+	go.opentelemetry.io/otel v1.16.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.16.0
-	go.opentelemetry.io/otel/trace v1.16.0
+	go.opentelemetry.io/otel/sdk v1.16.0
 	go.uber.org/automaxprocs v1.5.3
 	go.uber.org/zap v1.24.0
 	golang.org/x/crypto v0.7.0
@@ -34,10 +35,9 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	github.com/hbollon/go-edlib v1.6.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	go.opentelemetry.io/otel v1.16.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
